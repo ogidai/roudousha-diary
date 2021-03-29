3e5595b25b3c0c756e8876da7397978585a8df6f
Daiki Ogikubo <ogikubo@exat.jp>
2021-03-29 14:31:01 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+<header>
+  <div class="header-inner">
+  <h1 class="header-logo">労働者日記</h1>
+  <span class="toggle-icon">
+    <b-icon v-b-toggle.my-sidebar icon="filter-right" font-scale="2"></b-icon>
+  </span>
+  <Sidebar/>
+  </div>
+</header>
+</template>
+
+<script>
+import Sidebar from './Sidebar.vue'
+
+export default {
+    name: 'Header',
+    components: {
+      Sidebar
+    },
+    data() {
+      return {
+        visible: false,
+      }
+    },
+    methods: {
+      hideSidebar() {
+        this.localShow = false
+        console.log(this.localShow)
+      }
+    }
+
+}
+</script>
+
+<style>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border-bottom: 1px solid #ddd;
+}
+.header-inner {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 10px 30px;
+}
+.header-logo {
+  padding: 0;
+  margin: 0;
+  width: 150px;
+  font-size: 30px;
+}
+.toggle-icon {
+  position: absolute;
+  top: 50%;
+  right: 30px;
+  transform: translateY(-50%);
+}
+svg {
+  outline: none;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,61 @@
+<template>
+        <b-sidebar id="my-sidebar" shadow width='80%' no-header>
+            <nav class="nav">
+            <span class="close-nav-icon">
+                <b-icon v-b-toggle.my-sidebar icon="x" font-scale="2"></b-icon>
+            </span>
+            <ul class="nav-list">
+                <li><a href="">ホーム</a></li>
+                <li><a href="">労働者情報</a></li>
+                <li><a href="">プライバシーポリシー</a></li>
+                <li><a href="">利用規約</a></li>
+                <li><a href="">お問い合わせ</a></li>
+                <li><a href=""><img src="../assets/line.png" alt="" class="line-icon"></a></li>
+            </ul>
+            </nav>
+        </b-sidebar>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.nav {
+    padding: 30px 15px;
+}
+.nav-list {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    list-style: none;
+}
+.nav-list li {
+    border-bottom: 1px solid #ddd;
+}
+.nav-list li:last-child {
+    border: none;
+}
+.nav-list li + li {
+    margin-top: 10px;
+}
+.nav-list a {
+    display: block;
+    padding: 5px 10px;
+    text-decoration: none;
+    color: #333;
+}
+.close-nav-icon {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+}
+.line-icon {
+    width: 30px;
+}
+</style>
